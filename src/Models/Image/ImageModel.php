54141356459559ebafedec6e154c174e59c69d94
veljko-d <veljko.d@protonmail.com>
2020-04-09 22:42:33 +0200

    Add getByPost method in ImageModel and related tests in ImageModelTest

--- src/Models/Image/ImageModel.php
+++ src/Models/Image/ImageModel.php
@@ -3,7 +3,10 @@
 namespace App\Models\Image;
 
 use App\Domain\Image;
+use App\Exceptions\DbException;
 use App\Models\AbstractModel;
+use PDO;
+use PDOException;
 
 /**
  * Class ImageModel
@@ -16,4 +19,28 @@ class ImageModel extends AbstractModel implements ImageModelInterface
      * @const
      */
     const CLASSNAME = Image::class;
+
+    /**
+     * @param int $postId
+     *
+     * @return array
+     * @throws DbException
+     */
+    public function getByPost(int $postId): array
+    {
+        $query = 'SELECT *
+            FROM images 
+            WHERE post_id = :post_id';
+
+        try {
+            return $this->db->fetchAll(
+                $query,
+                [':post_id' => $postId],
+                PDO::FETCH_CLASS,
+                self::CLASSNAME
+            );
+        } catch (PDOException $e) {
+            throw new DbException($e->getMessage());
+        }
+    }
 }
--- tests/Unit/Models/ImageModelTest.php
+++ tests/Unit/Models/ImageModelTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models;
 
+use App\Exceptions\DbException;
 use App\Models\Image\ImageModel;
 use Tests\Unit\ModelTestCase;
 
@@ -32,5 +33,53 @@ class ImageModelTest extends ModelTestCase
         parent::setUp();
 
         $this->imageModel = new ImageModel($this->db);
+    }
+
+    /**
+     * @throws DbException
+     */
+    public function testGetImagesByPostArrayEmpty()
+    {
+        $images = $this->imageModel->getByPost(3);
+
+        $this->assertEmpty(
+            $images,
+            "Array is not empty."
+        );
+    }
+
+    /**
+     * @throws DbException
+     */
+    public function testGetImagesByPost()
+    {
+        $this->addImage();
+
+        $images = $this->imageModel->getByPost(2);
+
+        $this->assertCount(
+            1,
+            $images,
+            "Array size not as expected."
+        );
+    }
+
+    /**
+     * Insert new image into the database
+     */
+    private function addImage()
+    {
+        $params = [
+            ':name'   => 'image-name',
+            ':ext'    => 'png',
+            ':size'   => 1000,
+            ':path'   => '/path/to/image',
+            ':postId' => 2,
+        ];
+
+        $query = 'INSERT INTO images (name, ext, path, size, post_id, created_at)
+			VALUES (:name, :ext, :path, :size, :postId, NOW())';
+
+        $this->db->execute($query, $params);
     }
 }
